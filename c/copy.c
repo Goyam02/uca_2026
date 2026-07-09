ca07900057adfb5e477664dcac2f1411c5d83bf2
Goyam02 <goyamjain02@gmail.com>
2026-07-09 18:14:30 +0000

    add: auditlog

--- c/copy.c
+++ c/copy.c
@@ -14,27 +14,53 @@ int main(int argc, char *argv[]){
 		printf("Usage ./a.out file1 file2");
 		exit(-1);	
 	}
-	inputFd = open(argv[1], O_RDONLY);
-	if(inputFd == -1) {
-		printf("Error opening file %s\n",argv[1]);
-		exit(-1);
-	}
-	openFlags = O_CREAT | O_WRONLY | O_TRUNC;
-	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // rw-r-r- 644
-	outputFd = open(argv[2],openFlags, filePerms);
-	if (outputFd == -1) {
-		printf("Error opening file %s\n",argv[2]);
-		exit(-1);
-	}
-	while ((numRead = read(inputFd,buf,BUF_SIZE))>0) {
-		if(write(outputFd, buf, numRead)!= numRead){
-			printf("Some problemm reading or writing");
+	if (strcmp(argv[1], "--add") == 0) {
+        if (argc != 3) {
+            printf("Error: Missing message to add.\n");
+            exit(-1);
+        }
+		openFlags = O_CREAT | O_WRONLY | O_APPEND;
+		filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // rw-r--r-- 644
+		outputFd = open(argv[2], openFlags, filePerms);
+		if (outputFd == -1) {
+			printf("Error opening output file %s\n", argv[2]);
+			exit(-1);
+		}
+		// Write the message to the log file
+		if (write(outputFd, argv[2], strlen(argv[2])) != strlen(argv[2])) {
+			printf("Error writing to output file %s\n", argv[2]);
+			close(outputFd);
+			exit(-1);
+		}
+		close(outputFd);
+	} else {
+		inputFd = open(argv[1], O_RDONLY);
+		if(inputFd == -1) {
+			printf("Error opening input file %s\n", argv[1]);
 			exit(-1);
 		}
 
+		openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+		filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH; // rw-r--r-- 644
+		outputFd = open(argv[2], openFlags, filePerms);
+		if (outputFd == -1) {
+			printf("Error opening output file %s\n", argv[2]);
+			close(inputFd);
+			exit(-1);
+		}
+
+		while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0) {
+			if(write(outputFd, buf, numRead) != numRead){
+				printf("Error writing to output file %s\n", argv[2]);
+				close(inputFd);
+				close(outputFd);
+				exit(-1);
+			}
+		}
+
+		close(inputFd);
+		close(outputFd);
 	}
-	close(inputFd);
-	close(outputFd);
 
 	//printf("%d\n",argc);
 	return 0;
